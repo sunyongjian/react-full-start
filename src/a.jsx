756110ccf17a8ea415e61d38c29c7a517eb33897
sunyongjian <704928775@qq.com>
2017-12-29 00:20:34 +0800

    chore: polyfill, env, eslint 添加了 env 的内建模式，加入 polyfill，并使用 async 函数测试

--- src/a.jsx
+++ src/a.jsx
@@ -5,20 +5,27 @@ export default class A extends React.PureComponent {
   constructor() {
     super();
     this.state = {
-      list: []
-    }
+      list: [],
+    };
   }
+
   componentDidMount() {
-    axios({
-      method: 'get',
-      url: '/api/list',
-      responseType: 'json'
-    })
-      .then(function (response) {
-        console.log(response);
+    (async () => {
+      const response = await axios({
+        method: 'get',
+        url: '/api/list',
+        responseType: 'json',
       });
+      console.log(response, 'response');
+      const { data: { list = [] } = {} } = response;
+      this.setState({
+        list,
+      });
+    })();
   }
+
   render() {
-    return <div>AAAAA</div>
+    const { list } = this.state;
+    return <ul>{list.length && list.map(item => (<li key={item.id}>{item.id + item.name}</li>))}</ul>;
   }
 }
